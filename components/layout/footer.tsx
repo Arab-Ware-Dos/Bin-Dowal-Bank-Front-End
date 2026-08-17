f715098cbc7ff2ba1f6f1d72f4f319313f4869bf
salem bashnddof <salembashndoof@gmail.com>
2026-08-17 13:44:45 +0300

    fix the footer image and href

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -248,14 +248,14 @@ export function Footer() {
               transition={{ duration: 0.25 }}
             >
               <a
-            href="https://bindowalgroup.com/"
+            // href="https://bindowalgroup.com/"
             target="_blank"
             rel="noopener noreferrer"
             className="group flex flex-col items-center gap-2.5 transition-opacity hover:opacity-80"
           >
               <Image
-                src="/images/bindowal-group.png"
-                alt="Bin Dowal Group"
+                  src="/images/dowal.png"
+                  alt="Dowal"
                 width={200}
                 height={100}
                 className="h-24 sm:h-20 md:h-32 w-auto object-contain opacity-90 transition-opacity hover:opacity-100"
